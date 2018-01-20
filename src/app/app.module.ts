07817e034da969926e575ec6277965745fc3dc92
Ujjwal Jindal <ujjwaljindal12@gmail.com>
2018-01-21 00:06:30 +0530

    Added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule, FormsModule, HttpModule
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
 
