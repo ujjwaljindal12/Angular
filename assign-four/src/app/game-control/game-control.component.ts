73913fb217e4a381d320c344f086f1b71bcd4037
Ujjwal Jindal <ujjwaljindal12@gmail.com>
2018-02-14 23:29:37 +0530

    changes in assign

---
+++ assign-four/src/app/game-control/game-control.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  startgame() {
+    console.log('Welcome');
+  }
+
+}
