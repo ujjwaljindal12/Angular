efa20d2e21d5810492b2a98dd582009a7d942dc7
Ujjwal Jindal <ujjwaljindal12@gmail.com>
2018-02-18 15:53:41 +0530

    assign four processing

--- assign-four/src/app/game-control/game-control.component.ts
+++ assign-four/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,14 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  startgame() {
-    console.log('Welcome');
+  onStartGame() {
+this.interval = setInterval(() => {
+  this.intervalFired.emit(this.lastNumber + 1);
+  this.lastNumber++;
+},1000)
   }
 
 }
